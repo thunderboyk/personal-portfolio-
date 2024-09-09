fc4cfb422d6bfac9085cf639862c62e769abcf35
thunderboyk <lakshyajun27@gmail.com>
2024-09-09 14:12:57 +0530

    Add files via upload
    creating the class and installing pip pillow as we are going to use images edit or change in models are called migrations telling the database there is something new and to install unintalled models pythong manage.py models and migrate never do this on the file use these comands

--- personal_portfolio/personalportfolio/models.py
+++ personal_portfolio/personalportfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+		title = models.CharField(max_length=100)
+		discription = models.CharField(max_length=250)
+		image = models.ImageField(upload_to='personalportfolio/image/')
+		url= models.URLField(blank=True)
+
+		
+
+    #not nessary for clicking is blank 
