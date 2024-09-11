40b28a479256f24f40c818cad05ab531e134778d
thunderboyk <lakshyajun27@gmail.com>
2024-09-11 23:55:51 +0530

    Add files via upload
    adding fetures like adding plural or single blog by blog.count

--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+# get object is for finding a object if it is not present then it gives 404 page
 from .models import Blogs
 # Create your views here.
 def all_blogs(request):
 
-	blogs = Blogs.objects.order_by("-date")[:5]
+	blogs = Blogs.objects.order_by("-date")
 #instead of showing all the blogs we can do order.by - date most current will show up 
 	return render(request,"blog/all_blogs.html" , {'blogs': blogs})
 
 
 
 def detail(request ,blog_id):
-	return render(request,'blog/detail.html',{'id':blog_id})
+	blog = get_object_or_404(Blogs,pk = blog_id)
+	return render(request,'blog/detail.html',{'blog':blog})
 
 
