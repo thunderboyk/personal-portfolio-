3ae76266a1b86e905cf4b65404155228fa876a59
thunderboyk <lakshyajun27@gmail.com>
2024-09-11 20:50:52 +0530

    Add files via upload
    how to get the objects we defined in the template

--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# for adding a path of media other than the default place
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL='media/'
--- personal_portfolio/personalportfolio/views.py
+++ personal_portfolio/personalportfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def home(request):
+	projects = Project.objects.all()
+ 	
+
+	return render(request,'port/home.html', {'projects':projects})
