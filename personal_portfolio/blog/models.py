d287a9f775996a1a4236605f93ec7eeead16f810
thunderboyk <lakshyajun27@gmail.com>
2024-09-12 22:12:36 +0530

    Add files via upload

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -5,5 +5,9 @@ from django.db import models
 # Create your models here.
 class Blogs(models.Model):
 		title = models.CharField(max_length=200)
-		discription = models.TextField(max_length=250)
+		discription = models.TextField(max_length=1000)
 		date = models.DateField()
+
+
+		def __str__(self):
+			return self.title
--- personal_portfolio/personalportfolio/models.py
+++ personal_portfolio/personalportfolio/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
 		title = models.CharField(max_length=100)
-		discription = models.CharField(max_length=250)
+		discription = models.CharField(max_length=1000)
 		image = models.ImageField(upload_to='personalportfolio/image/')
 		url= models.URLField(blank=True)
+
+
+		def __str__(self):
+			return self.title
+			# for showing the title of the projects on the admin page 
 
 		
 
